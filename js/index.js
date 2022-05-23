eac31bba051be28c2967a281586a4a79f43d467b
Ankit Kanyal <ankitsinghkanyal43@gmail.com>
2022-05-23 16:45:20 +0530

    Added js logic for working of calculator

--- js/index.js
+++ js/index.js
@@ -1,24 +1,77 @@
 const input = document.querySelector("input");
+console.log("input " + input.value !== "0");
 
 document.querySelectorAll(".num__key").forEach((e1) => {
-  e1.onclick = () =>
-    input.value !== "0" ? input.value + e1.innerText : e1.innerText;
+  e1.onclick = () => {
+    input.value =
+      input.value !== "0" ? input.value + e1.innerText : e1.innerText;
+    console.log("Inside key " + input.value);
+  };
 });
 
 const buffer = [];
 
-const opCallBack = opName => () =>{
-   let currentVal=parseFloat(input.value);
-   if(opName==="percent"){
-       currentVal *=0.01;
-       input.value=currentVal;
-   } 
+const opCallBack = (opName) => () => {
+  let currentVal = parseFloat(input.value);
+  if (opName === "percent") {
+    currentVal *= 0.01;
+    input.value = currentVal;
+  } else {
+    if (buffer && buffer.length) {
+      buffer.push({ value: currentVal });
 
-   else{
-       if(buffer)
-   }
-}
+      const result = evaluate(buffer);
 
-for (const opName of ["add", "subtract", "multiply", "divide", "precent"]) {
+      buffer.push({ value: result });
+      buffer.push({ value: opName });
+
+      input.value = "";
+    } else {
+      buffer.push({ value: currentVal });
+      buffer.push({ value: opName });
+      input.value = "";
+    }
+  }
+};
+
+const evaluate = (buffer) => {
+  const secondOperand = buffer.pop().value;
+  const operator = buffer.pop().value;
+  const firstOperand = buffer.pop().value;
+
+  switch (operator) {
+    case "add":
+      return firstOperand + secondOperand;
+      break;
+    case "subtract":
+      return firstOperand - secondOperand;
+      break;
+    case "multiply":
+      return firstOperand * secondOperand;
+      break;
+    case "divide":
+      return firstOperand / secondOperand;
+      break;
+    default:
+      return secondOperand;
+  }
+};
+
+for (const opName of ["add", "subtract", "multiply", "divide", "percent"]) {
   document.querySelector(`.op__key[op=${opName}]`).onclick = opCallBack(opName);
 }
+
+document.querySelector(".eq__key").onclick = () => {
+  if (buffer && buffer.length) {
+    buffer.push({ value: parseFloat(input.value) });
+    input.value = evaluate(buffer);
+  }
+};
+
+document.querySelector(".op__key[op=clear]").onclick = () => {
+  input.value = 0;
+  buffer.length = 0;
+};
+
+document.querySelector(".op__key[op=negate]").onclick = () =>
+  (input.value = -parseFloat(input.value));
